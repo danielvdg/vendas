447294dc217f43e23b16703d505bea88650e4bf4
Daniel Alves <daniel.vdg@hotmail.com>
2023-09-06 21:48:24 -0300

    autendicação com spring security

---
+++ src/main/java/br/com/vendas/controller/UsuarioController.java
@@ -0,0 +1,28 @@
+package br.com.vendas.controller;
+
+import br.com.vendas.model.entity.Usuario;
+import br.com.vendas.service.impl.UsuarioServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioServiceImpl usuarioService;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Usuario salvar(@RequestBody @Valid Usuario usuario) {
+        String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(senhaCriptografada);
+        return  usuarioService.salvar(usuario);
+    }
+
+}
---
+++ src/main/java/br/com/vendas/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package br.com.vendas.repository;
+
+import br.com.vendas.model.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+
+    Optional<Usuario> findByLogin(String login);
+
+}
